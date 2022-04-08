334c2e4c9dd0eb839cc20b62a75e9051245b4ab9
HITMANdark07 <shekharkumar235838@gmail.com>
2022-04-08 20:56:06 +0530

    backend charging schema

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Devices from './pages/Devices/Devices';
+import History from './pages/History/History';
 import Home from './pages/Home/Home';
-import Products from './pages/Products/Products';
+import Transaction from './pages/Transaction/Transaction';
 import Users from './pages/Users/Users';
 
 function App() {
@@ -17,7 +18,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path='/devices' element={<Devices />} />
         <Route path='/users' element={<Users  />} />
-        <Route path='/products' element={<Products  />} />
+        <Route path='/transactions' element={<Transaction  />} />
+        <Route path='/history' element={<History  />} />
       </Routes>
     </Router>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { ShieldCheckIcon } from "@heroicons/react/solid";
 import { Link,useLocation, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "../redux/user/user.action";
+import { AiOutlineBank,AiOutlineHistory } from 'react-icons/ai';
 
 const Sidebar = ({activeMenu}) => {
 
@@ -100,22 +101,20 @@ const Sidebar = ({activeMenu}) => {
           </li>
           <li>
             <Link
-              to="/products"
+              to="/history"
               className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white ${location.pathname==="/products" && "bg-gray-700" } hover:bg-gray-100 dark:hover:bg-gray-700`}
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
+              <AiOutlineHistory color="grey" size={20} />
+              <span className="flex-1 ml-3 whitespace-nowrap">Charging History</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/transactions"
+              className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white ${location.pathname==="/products" && "bg-gray-700" } hover:bg-gray-100 dark:hover:bg-gray-700`}
+            >
+              <AiOutlineBank color="grey" size={20} />
+              <span className="flex-1 ml-3 whitespace-nowrap">Transactions</span>
             </Link>
           </li>
           <li>
